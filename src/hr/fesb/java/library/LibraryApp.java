1ef2e8a97431c0680ad426f4a06a8739c6d12f75
jules <jules.roulleau@gmail.com>
2026-04-21 23:34:39 +0200

    project structure, read me, gitignore, first compilating file

---
+++ src/hr/fesb/java/library/LibraryApp.java
@@ -0,0 +1,8 @@
+package hr.fesb.java.library;
+
+
+public class LibraryApp {
+    public static void main(String[] args) {
+        System.out.println("Library Information System - Launching");
+    }
+}
